46ee5fa7aef589a08dbc454a4b669ad97d20b103
Alberto Zordan <zorberto@gmail.com>
2020-07-16 17:52:30 +0200

    Functions done

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@
 // Prezzo calcolato
 // Categoria selezionata dall'utente
 // Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
+
+// FUNZIONI
+// a. ASSEGNAZIONE FASCIA PREZZO
+function discount(category) {
+    var offer = 'Prezzo intero';
+    if (category == minor) {
+        offer = 'Silver: -20%';
+    } else if (category == adult) {
+        offer = 'Gold: -40%';
+    }
+    return offer;
+}
+
+// b. GENERAZIONE NUMERI RANDOM
+function randomNum(min,max) {
+    return Math.floor(Math.random()*(max - min + 1) + min);
+}
+
+// c. CALCOLO PREZZO BIGLIETTO
+function ticketPrice(km,eta) {
+    var prezzo = 0.21*km;
+    if (eta == minor) {
+        prezzo -= 0.20*prezzo;
+    }   else if (eta == over) {
+        prezzo -= 0.40*prezzo;
+    }
+    return prezzo.toFixed(2);
+}
