32939a2342a3c17d059cd5eb3e1efd9a18dc9f4c
Alberto Zordan <zorberto@gmail.com>
2020-07-21 17:04:34 +0200

    Some improvements and styling added

--- js/script.js
+++ js/script.js
@@ -8,28 +8,33 @@
 
 // 1. Descrizione dell'evento che si verifica cliccando sul button "Genera"
 document.getElementById('genera').addEventListener('click',function() {
+    // Salvo in delle variabili i valori acquisiti da input
+    var utente = document.getElementById('full-name-input').value;
+    var km = document.getElementById('km-input').value;
+    var eta = document.getElementById('age-input').value;
+    
+    // Imposto un blocco alla funzione qualora l'utente non inserisca tutti i dati 'required'
+    if (utente == '' || km == '' || eta == '') {
+        return
+    }
+
     // Creazione del biglietto: al click aggiunta della classe 'show' e rimozione della classe 'hidden'
     var biglietto = document.getElementById('ticket-print');
     biglietto.classList.add('show');
     biglietto.classList.remove('hidden');
 
-    // Salvo in delle variabili i valori acquisiti da input
-    var utente = document.getElementById('full-name-input').value;
-    var km = document.getElementById('km-input').value;
-    var eta = document.getElementById('age-input').value;
-
     // Invoco le varie funzioni per salvare in delle variabili i valori che andrò a stampare a video
     var prezzo = ticketPrice(km,eta);
     var offerta = discount(eta);
     var carrozza = randomNum(1,10);
-    var codice = randomNum(90000,99999);
+    var codice = randomNum(90000,100000);
 
     // Stampa a video dei dati di output
     document.getElementById('full-name-output').innerHTML = utente;
     document.getElementById('offer').innerHTML = offerta;
     document.getElementById('carriage').innerHTML = carrozza;
     document.getElementById('code').innerHTML = codice;
-    document.getElementById('price').innerHTML = prezzo;
+    document.getElementById('price').innerHTML = prezzo + ' €';
 });
 
 // 2. Descrizione dell'evento che si verifica cliccando sul button "Annulla"
@@ -41,8 +46,8 @@ document.getElementById('annulla').addEventListener('click',function() {
 
     // Reset campi di input
     document.getElementById('full-name-input').value = '';
-    document.getElementById('km-input').value = 0;
-    document.getElementById('age-input').value = 'Minorenne';
+    document.getElementById('km-input').value = '';
+    document.getElementById('age-input').value = '';
 });
 
 // FUNZIONI
@@ -59,7 +64,7 @@ function discount(category) {
 
 // b. GENERAZIONE NUMERI RANDOM
 function randomNum(min,max) {
-    return Math.floor(Math.random()*(max - min + 1) + min);
+    return Math.floor(Math.random()*(max - min) + min);
 }
 
 // c. CALCOLO PREZZO BIGLIETTO
